e4eacb1dd19c50c13d751654efd8cba97099169e
uzair-boot <ozairahmad984@gmail.com>
2022-09-21 18:47:34 +0500

    GraphQl endPoint data fetched and displayed in table

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,19 @@
+import React from 'react';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://profound-marmot-29.hasura.app/v1/graphql',
+  cache: new InMemoryCache(),
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
